dd22b34cb001aa5db2b0c429fd6175023956294c
Sergio Vieira <sergiovieira@gmail.com>
2018-06-12 08:53:48 -0300

    - Updated airplane constructor

--- airplane.cpp
+++ airplane.cpp
@@ -9,8 +9,9 @@ AirPlane::AirPlane
     const Coordinates &position,
     const Vector &velocity,
     unsigned int initialTime,
+    unsigned int lifeTime,
     const Vector& acceleration
-): Object(position, velocity, initialTime, acceleration, ObjectType::AirPlane)
+): Object(position, velocity, initialTime, lifeTime, acceleration, ObjectType::AirPlane)
 {
 
 }
