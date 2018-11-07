00e055d12cb8c35c6a5c7bedc97d4ffcef53ac85
Sergio Vieira <sergiovieira@gmail.com>
2018-11-07 11:19:16 -0300

    - Adds eigen lib

--- erase.sh
+++ erase.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-array=("*.pro.user" "*.moc" "*.pro.user.*" "*.qbs.user" "*.qbs.user.*" "moc_*.cpp" "moc_*.h" "qrc_*.cpp" "ui_*.h" "*.qmlc" "*.o")
-for f in "${array[@]}˜"
-do
-	find . -iname "$f" -exec rm {} \; -print
-done
-array=(mog.app html latex rtf)
+array=(*.pro.user *.moc *.pro.user.* *.qbs.user *.qbs.user.* moc_*.cpp moc_*.h qrc_*.cpp ui_*.h *.qmlc *.o mog.app html latex rtf)
 for f in "${array[@]}"
 do
 	rm -rf $f
